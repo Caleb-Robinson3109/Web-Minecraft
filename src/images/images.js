c1fef0662b5f340676bf76be0ba63f81b8e0da31
Caleb Robinson <cmrd2d@umsystem.edu>
2024-12-16 22:24:23 -0600

    config

---
+++ src/images/images.js
@@ -0,0 +1,15 @@
+import diamondImg from './diamond.png';
+import dirtImg from './dirt.png';
+import glassImg from './glass.png';
+import grassImp from './grass.png';
+import woodImg from './wood.png';
+
+const images = {
+    diamondImg,
+    dirtImg,
+    glassImg,
+    grassImp,
+    woodImg,
+}
+
+export default images;
